07971f497e15297a9983ac754c798544ca650dce
zsp00 <zhusipu123@163.com>
2023-10-25 15:56:31 +0000

    commit new code

--- 1/38A/0x38A0df9a08d18dc06CD91Fc7Ec94a0AcdF28D994/TokenRecipient.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.11;
-
-import "./Pausable.sol";
-
-contract TokenRecipient {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes memory _extraData) public;
-}
